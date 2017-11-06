1142c8d93c69cfae1faf5679f59a73942f0cc3e6
Marius Maaland <mariusmaaland@gmail.com>
2017-11-06 11:01:00 +0100

    add FormsModule, HttpModule, RouterTestingModule and CompanyService to company detail spec to pass test

--- glassdoor/src/app/companies/company-detail/company-detail.component.spec.ts
+++ glassdoor/src/app/companies/company-detail/company-detail.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterTestingModule } from '@angular/router/testing'
+
 
 import { CompanyDetailComponent } from './company-detail.component';
+import { CompanyService } from '../shared/company.service';
+
 
 describe('CompanyDetailComponent', () => {
   let component: CompanyDetailComponent;
@@ -8,7 +14,9 @@ describe('CompanyDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CompanyDetailComponent ]
+      declarations: [ CompanyDetailComponent ],
+      imports: [ FormsModule, HttpModule, RouterTestingModule ],
+      providers: [ CompanyService ]
     })
     .compileComponents();
   }));
