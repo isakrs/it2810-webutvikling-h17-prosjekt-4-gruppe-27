42b9a14c448f41ecee606953181559851d3298db
Marius Maaland <mariusmaaland@gmail.com>
2017-11-08 19:01:23 +0100

    remove spy service from company detail

--- glassdoor/src/app/companies/company-detail/company-detail.component.spec.ts
+++ glassdoor/src/app/companies/company-detail/company-detail.component.spec.ts
@@ -6,7 +6,6 @@ import { RouterTestingModule } from '@angular/router/testing'
 
 import { CompanyDetailComponent } from './company-detail.component';
 import { CompanyService } from '../shared/company.service';
-import { CompanyServiceSpy } from '../../dashboard/company-service-spy.service';
 
 
 
@@ -14,35 +13,21 @@ describe('CompanyDetailComponent', () => {
   let component: CompanyDetailComponent;
   let fixture: ComponentFixture<CompanyDetailComponent>;
 
-   let csSpy: CompanyServiceSpy;
 
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CompanyDetailComponent ],
       imports: [ FormsModule, HttpModule, RouterTestingModule ],
+      providers: [ CompanyService ]
     })
-    // Override component's own provider
-    .overrideComponent(CompanyDetailComponent, {
-      set: {
-        providers: [
-          {provide: CompanyService, useClass: CompanyServiceSpy}
-        ]
-      }
-    })
+   
     .compileComponents().then( () =>  {
       fixture = TestBed.createComponent(CompanyDetailComponent);
       component = fixture.componentInstance;
-      csSpy = fixture.debugElement.injector.get(CompanyService) as any;
     })
   }));
-
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(CompanyDetailComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
-
+  
   it('should create', () => {
     fixture.detectChanges();
     console.log("IN THIS TEST*********")
