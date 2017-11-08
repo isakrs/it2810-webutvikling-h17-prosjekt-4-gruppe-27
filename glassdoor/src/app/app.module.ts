8808bba20b758852627566624f5933d59bc2fc2d
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-08 18:48:11 +0100

    Add new line to add air between imports of services and components. For legibility.

--- glassdoor/src/app/app.module.ts
+++ glassdoor/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpModule }    from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
-// Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './shared/in-memory-data.service';
 
@@ -14,6 +13,7 @@ import { CompaniesComponent } from './companies/companies.component';
 import { CompanyDetailComponent } from './companies/company-detail/company-detail.component';
 import { CompanySearchComponent } from './companies/company-search/company-search.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { CompanyService } from './companies/shared/company.service';
 
 @NgModule({
