881e96283fde350142cf928d8e47d5b77ca650be
Marius Maaland <mariusmaaland@gmail.com>
2017-11-08 19:45:19 +0100

    add query and mock response tests to company.service

--- glassdoor/src/app/companies/shared/company.service.spec.ts
+++ glassdoor/src/app/companies/shared/company.service.spec.ts
@@ -1,18 +1,51 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpModule } from '@angular/http';
-import { RouterTestingModule } from '@angular/router/testing'
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Http, Response, ResponseOptions, BaseRequestOptions, RequestOptions, ConnectionBackend } from '@angular/http';
+import { ReflectiveInjector } from '@angular/core';
 
 import { CompanyService } from './company.service';
+import { Company } from './company.model';
 
 describe('CompanyService', () => {
+
+	const mockResponse = [
+    {id: 0, name: 'DNB'},
+    {id: 1, name: 'Bekk'}
+  ];
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CompanyService],
-      imports: [ HttpModule, RouterTestingModule ]
-    });
+    this.injector = ReflectiveInjector.resolveAndCreate([
+      {provide: ConnectionBackend, useClass: MockBackend},
+      {provide: RequestOptions, useClass: BaseRequestOptions},
+      Http,
+      CompanyService
+    ]);
+
+    this.companyService = this.injector.get(CompanyService);
+    this.backend = this.injector.get(ConnectionBackend) as MockBackend;
+    this.backend.connections.subscribe((connection: any) => this.lastConnection = connection);
   });
 
-  it('should be created', inject([CompanyService], (service: CompanyService) => {
-    expect(service).toBeTruthy();
+  it('should query current service url [company.service]', () => {
+    this.companyService.getCompanies();
+    expect(this.lastConnection).toBeDefined();
+    expect(this.lastConnection.request.url).toBe('api/companies');
+  });
+
+  it('getCompanies() should return companies', fakeAsync(() => {
+
+    let result: Company[];
+    
+    // note: had to add '.data' to companies here, if not result was undefined. Ref earlier discussion about json().data
+    this.companyService.getCompanies().then((companies) => result = companies.data);
+    this.lastConnection.mockRespond(new Response(new ResponseOptions({
+      body: JSON.stringify({data: mockResponse})
+    })));
+
+    tick();
+    expect(result.length).toBe(2);
+    expect(result[0].id).toBe(0);
+    expect(result[0].name).toBe('DNB');
+
   }));
 });
