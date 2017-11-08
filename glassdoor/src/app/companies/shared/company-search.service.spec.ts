2b84ce69ef205c65780b9a266a9090d78303feca
Marius Maaland <mariusmaaland@gmail.com>
2017-11-08 19:16:16 +0100

    add backend mocking tests to company-search.service

--- glassdoor/src/app/companies/shared/company-search.service.spec.ts
+++ glassdoor/src/app/companies/shared/company-search.service.spec.ts
@@ -1,18 +1,28 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpModule } from '@angular/http';
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { ReflectiveInjector } from '@angular/core';
+import { Http, Response, ResponseOptions, BaseRequestOptions, RequestOptions, ConnectionBackend } from '@angular/http';
 
 
 import { CompanySearchService } from './company-search.service';
 
 describe('CompanySearchService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CompanySearchService],
-      imports: [ HttpModule ]
-    });
+    this.injector = ReflectiveInjector.resolveAndCreate([
+      {provide: ConnectionBackend, useClass: MockBackend},
+      {provide: RequestOptions, useClass: BaseRequestOptions},
+      Http,
+      CompanySearchService
+    ]);
+
+    this.companySearchService = this.injector.get(CompanySearchService);
+    this.backend = this.injector.get(ConnectionBackend) as MockBackend;
+    this.backend.connections.subscribe((connection: any) => this.lastConnection = connection);
   });
 
-  it('should be created', inject([CompanySearchService], (service: CompanySearchService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should query current service url', () => {
+    this.companySearchService.search('test');
+    expect(this.lastConnection).toBeDefined();
+    expect(this.lastConnection.request.url).toBe('api/companies/?name=test');
+  });
 });
