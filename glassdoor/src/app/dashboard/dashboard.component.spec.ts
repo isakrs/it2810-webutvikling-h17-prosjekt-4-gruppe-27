31edaac7a3b383787767346cb257cf4a8306ca79
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-08 11:28:08 +0100

    Add title test to dasboard.component.spec.ts.

--- glassdoor/src/app/dashboard/dashboard.component.spec.ts
+++ glassdoor/src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { HttpModule } from '@angular/http';
+import { RouterTestingModule } 	from '@angular/router/testing';
+import { HttpModule } 					from '@angular/http';
+import { By }										from '@angular/platform-browser';
+import { DebugElement }					from '@angular/core';
 
 import { DashboardComponent } from './dashboard.component';
 import { CompanySearchComponent } from '../companies/company-search/company-search.component';
 import { CompanyService } from '../companies/shared/company.service';
 
 describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+ 
+ 	let component: 	DashboardComponent;
+  let fixture: 		ComponentFixture<DashboardComponent>;
+	let de:					DebugElement;
+	let el:					HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,10 +27,19 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+		de = fixture.debugElement.query(By.css('h3'));
+		el = de.nativeElement;
+
+		fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+	it(`should display title 'Top Companies'`, () => {
+		expect(el.textContent).toEqual('Top Companies');
+	})
+
 });
