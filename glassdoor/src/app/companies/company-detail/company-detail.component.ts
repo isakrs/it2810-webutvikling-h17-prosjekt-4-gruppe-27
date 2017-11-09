3af1f67ffeafc0a6cbaf808e55965c316368a5d3
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-09 19:56:45 +0100

    Code layout.

--- glassdoor/src/app/companies/company-detail/company-detail.component.ts
+++ glassdoor/src/app/companies/company-detail/company-detail.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit }        from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location }                 from '@angular/common';
 
-import { Company }    	from '../shared/company.model';
+import { Company }    	    from '../shared/company.model';
 import { CompanyService } 	from '../shared/company.service';
 
 @Component({
