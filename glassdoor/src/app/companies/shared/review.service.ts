2704386b27711f0d4d4e492c0cdcf6d53d18df26
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-09 15:53:11 +0100

    Reuse code from company.service.ts to review.service.ts. To avoid errors.

--- glassdoor/src/app/companies/shared/review.service.ts
+++ glassdoor/src/app/companies/shared/review.service.ts
@@ -1,8 +1,56 @@
-import { Injectable } from '@angular/core';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Review } from './review.model';
 
 @Injectable()
 export class ReviewService {
 
-  constructor() { }
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private reviewsUrl = 'api/reviews';  // URL to web api
 
+  constructor(private http: Http) { }
+
+  getReviews(idCompany: number): Promise<Review> {
+    const url = `${this.reviewsUrl}/${idCompany}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json() as Review)
+      .catch(this.handleError);
+  }
+
+  delete(id: number): Promise<Review> {
+    const url = `${this.reviewsUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  create(rating: number, comment: string): Promise<Review> {
+    return this.http
+      .post(
+        this.reviewsUrl,
+        JSON.stringify({rating: rating, comment: comment}),
+        {headers: this.headers})
+      .toPromise()
+      .then(res => res.json() as Review)
+      .catch(this.handleError);
+  }
+
+  update(review: Review): Promise<Review> {
+    const url = `${this.reviewsUrl}/${review.id}`;
+    return this.http
+      .put(url, JSON.stringify(review), {headers: this.headers})
+      .toPromise()
+      .then(() => review)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
