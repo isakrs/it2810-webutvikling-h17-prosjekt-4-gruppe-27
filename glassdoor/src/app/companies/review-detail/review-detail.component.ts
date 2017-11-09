791e8aa913bd832a9a2c948534081fd22f867c91
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-09 15:31:53 +0100

    Import review model in ReviewDetailComponent.

--- glassdoor/src/app/companies/review-detail/review-detail.component.ts
+++ glassdoor/src/app/companies/review-detail/review-detail.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Review } from '../shared/review.model';
+
 @Component({
   selector: 'app-review-detail',
   templateUrl: './review-detail.component.html',
   styleUrls: ['./review-detail.component.css']
 })
 export class ReviewDetailComponent implements OnInit {
+  review: Review;
 
   constructor() { }
 
