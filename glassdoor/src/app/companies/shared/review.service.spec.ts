cf4056f0fdc9e07421fd6a38de3e199c974adaf4
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-09 16:08:14 +0100

    Add spec tests to review.service.

--- glassdoor/src/app/companies/shared/review.service.spec.ts
+++ glassdoor/src/app/companies/shared/review.service.spec.ts
@@ -1,15 +1,49 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { MockBackend, MockConnection } 			from '@angular/http/testing';
+import { Http, Response, ResponseOptions,
+				 BaseRequestOptions, RequestOptions,
+				 ConnectionBackend } 								from '@angular/http';
+import { ReflectiveInjector } 							from '@angular/core';
 
-import { ReviewService } from './review.service';
+import { ReviewService } 	from './review.service';
+import { Review } 					from './review.model';
 
 describe('ReviewService', () => {
+
+	const mockResponse = [
+    {id: 0, rating: 5, comment: 'DNB paid me well', idCompany: 1},
+    {id: 1, rating: 1, comment: 'paid shit.', idCompany: 1}
+  ];
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ReviewService]
-    });
+    this.injector = ReflectiveInjector.resolveAndCreate([
+      {provide: ConnectionBackend, useClass: MockBackend},
+      {provide: RequestOptions, useClass: BaseRequestOptions},
+      Http,
+      ReviewService
+    ]);
+
+    this.reviewService = this.injector.get(ReviewService);
+    this.backend = this.injector.get(ConnectionBackend) as MockBackend;
+    this.backend.connections.subscribe((connection: any) => this.lastConnection = connection);
   });
 
-  it('should be created', inject([ReviewService], (service: ReviewService) => {
-    expect(service).toBeTruthy();
+  it('getReviews(idCompany=0) should return reviews', fakeAsync(() => {
+
+    let result: Review[];
+
+    // note: had to add '.data' to companies here, if not result was undefined. Ref earlier discussion about json().data
+    this.reviewService.getReviews(1).then((reviews) => result = reviews.data);
+    this.lastConnection.mockRespond(new Response(new ResponseOptions({
+      body: JSON.stringify({data: mockResponse})
+    })));
+
+    tick();
+    expect(result.length).toBe(2);
+    expect(result[0].id).toBe(0);
+    expect(result[0].idCompany).toBe(1);
+    expect(result[0].rating).toBe(5);
+    expect(result[1].comment).toBe('paid shit.');
+
   }));
 });
