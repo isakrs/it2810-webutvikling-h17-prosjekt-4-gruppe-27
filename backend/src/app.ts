c35f71d1cc55f9e7483105743c833b75fa551a68
Johannes Z. Vollan <johannzv@stud.ntnu.no>
2017-11-14 00:28:04 +0100

    apply middelware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,33 @@
 import * as express from "express"
+import mongoose = require('mongoose')
+import * as promise from 'promise'
+import apiRouter from './controllers/index'
+import * as morgan from 'morgan'
 
 let port = process.env.PORT || 3000
-const app = express()
+let app = express()
+
+//register middleware
+app.use('/api', apiRouter)
+app.use(morgan('common'))
+let connectionString = 'mongodb://applicationUser:ehysb7TXc2G7esapvF@it2810-27.idi.ntnu.no:27017/glassdoor'
+mongoose.Promise = promise
 
 
-app.get("/", (req, res) => {
-    res.sendStatus(200).send()
-})
+const init = async()=>{
+    try{
+        await mongoose.connect(connectionString)
+        console.log('connected')
+    }catch(e){
+        console.log('could not connect to db')
+    }
+    try{
+        await app.listen(port)
+        console.log('server is listening on port: ', port)
+    }catch(e){
+        console.log('could not start server')
+    }
+}
 
-app.listen(port,()=>{
-    console.log("server running on port: ",port)
-})
-
+init()
 // comment
