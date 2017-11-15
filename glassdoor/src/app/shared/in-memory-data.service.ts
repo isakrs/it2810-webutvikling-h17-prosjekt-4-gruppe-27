5f29e76437fe90494a2b99ea623b4f1de5da43b3
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-15 09:56:53 +0100

    Add fields 'averageRating' and 'nComments' to frontend database.

--- glassdoor/src/app/shared/in-memory-data.service.ts
+++ glassdoor/src/app/shared/in-memory-data.service.ts
@@ -2,15 +2,15 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const companies = [
-      {id: 1, name: 'DNB'},
-      {id: 2, name: 'Bekk'},
-      {id: 3, name: 'Kolonial.no'},
-      {id: 4, name: 'Blank'},
-      {id: 5, name: 'Accenture'},
-      {id: 7, name: 'Mnemonic'},
-      {id: 8, name: 'Statoil'},
-      {id: 9, name: 'Microsoft'},
-      {id: 10, name: 'Palantir'}
+      {id: 1,   name: 'DNB',          averageRating: 3.4,   nComments: 12},
+      {id: 2,   name: 'Bekk',         averageRating: null,  nComments: null},
+      {id: 3,   name: 'Kolonial.no',  averageRating: 4.1,   nComments: 12},
+      {id: 4,   name: 'Blank',        averageRating: 5,     nComments: 2},
+      {id: 5,   name: 'Accenture',    averageRating: null,  nComments: null},
+      {id: 7,   name: 'Mnemonic',     averageRating: null,  nComments: null},
+      {id: 8,   name: 'Statoil',      averageRating: 4.5,   nComments: 8},
+      {id: 9,   name: 'Microsoft',    averageRating: 4.7,   nComments: 10},
+      {id: 10,  name: 'Palantir',     averageRating: null,  nComments: null}
     ];
     const reviews = [
       {id: 0, rating: 5, comment: 'Loved working here.', idCompany: 3},
