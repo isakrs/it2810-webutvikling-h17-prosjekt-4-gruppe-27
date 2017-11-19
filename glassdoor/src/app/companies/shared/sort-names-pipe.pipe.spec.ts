9237818c99d7020303832a4dc115473d85702db0
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-19 18:20:28 +0100

    Generate sortNamesPipe.

---
+++ glassdoor/src/app/companies/shared/sort-names-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SortNamesPipePipe } from './sort-names-pipe.pipe';
+
+describe('SortNamesPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SortNamesPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
