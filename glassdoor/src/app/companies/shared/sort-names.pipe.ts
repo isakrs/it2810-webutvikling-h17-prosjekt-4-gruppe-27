aa7bc88e58f12dce667472422b787d5ad44baeb0
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-19 20:01:24 +0100

    Allow user to sort companies on different values.

--- glassdoor/src/app/companies/shared/sort-names.pipe.ts
+++ glassdoor/src/app/companies/shared/sort-names.pipe.ts
@@ -7,19 +7,21 @@ import { Company } from './company.model';
 })
 export class SortNamesPipe implements PipeTransform {
 
-  transform(companies: Company[]): Company[] {
+  transform(companies: Company[], sortValue: any): Company[] {
+
     if (!companies || companies === undefined || companies.length === 0) return null;
 
       companies.sort((a: Company, b: Company) => {
-        if (a.name < b.name) {
+        if (a[sortValue] < b[sortValue]) {
           return -1;
-        } else if (a.name > b.name) {
+        } else if (a[sortValue] > b[sortValue]) {
           return 1;
         } else {
           return 0;
         }
       });
       return companies;
+
   }
 
 }
