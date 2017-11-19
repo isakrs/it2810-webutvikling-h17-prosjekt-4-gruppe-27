7d6963caefc7712ee58dc3b41d773a9822d9e561
Johannes Z. Vollan <johannzv@stud.ntnu.no>
2017-11-19 19:21:21 +0100

    add export of express app fix deprecated warning with mongoose.connect

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,22 +4,25 @@ import * as promise from 'promise'
 import apiRouter from './controllers/index'
 import * as morgan from 'morgan'
 
+
 let port = process.env.PORT || 3000
 let app = express()
+let connectionString = 'mongodb://applicationUser:ehysb7TXc2G7esapvF@it2810-27.idi.ntnu.no:27017/glassdoor'
 
 //register middleware
 app.use('/api', apiRouter)
 app.use(morgan('common'))
-let connectionString = 'mongodb://applicationUser:ehysb7TXc2G7esapvF@it2810-27.idi.ntnu.no:27017/glassdoor'
+
 mongoose.Promise = promise
 
 
 const init = async()=>{
     try{
-        await mongoose.connect(connectionString)
+        await mongoose.connect(connectionString,{useMongoClient: true})
         console.log('connected')
     }catch(e){
         console.log('could not connect to db')
+        process.exit(1)
     }
     try{
         await app.listen(port)
@@ -30,4 +33,5 @@ const init = async()=>{
 }
 
 init()
+export default app;
 // comment
