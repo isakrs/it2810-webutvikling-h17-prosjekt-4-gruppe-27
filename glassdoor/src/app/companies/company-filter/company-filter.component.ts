01afdf372b6ac9a835fa51f6f0eaa24af36d7d6f
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-20 12:57:40 +0100

    Generate new component company-filter.

---
+++ glassdoor/src/app/companies/company-filter/company-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-filter',
+  templateUrl: './company-filter.component.html',
+  styleUrls: ['./company-filter.component.css']
+})
+export class CompanyFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
