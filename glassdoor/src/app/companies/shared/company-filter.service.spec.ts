5d463c2e07ea9f7651515db864760aac1b6f4610
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-20 13:37:34 +0100

    Add url encoded GET requests for company-filter.service, with applicable tests.

--- glassdoor/src/app/companies/shared/company-filter.service.spec.ts
+++ glassdoor/src/app/companies/shared/company-filter.service.spec.ts
@@ -1,15 +1,47 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, tick }       from '@angular/core/testing';
+import { MockBackend, MockConnection }            from '@angular/http/testing';
+import { ReflectiveInjector }                     from '@angular/core';
+import {
+  Http,
+  Response,
+  ResponseOptions,
+  BaseRequestOptions,
+  RequestOptions,
+  ConnectionBackend
+  } from '@angular/http';
 
 import { CompanyFilterService } from './company-filter.service';
 
 describe('CompanyFilterService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [CompanyFilterService]
-    });
+    this.injector = ReflectiveInjector.resolveAndCreate([
+      {provide: ConnectionBackend, useClass: MockBackend},
+      {provide: RequestOptions, useClass: BaseRequestOptions},
+      Http,
+      CompanyFilterService
+    ]);
+
+    this.CompanyFilterService = this.injector.get(CompanyFilterService);
+    this.backend = this.injector.get(ConnectionBackend) as MockBackend;
+    this.backend.connections.subscribe((connection: any) => this.lastConnection = connection);
   });
 
-  it('should be created', inject([CompanyFilterService], (service: CompanyFilterService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should query current service url with only minRating', () => {
+    this.CompanyFilterService.search(4.4, undefined);
+    expect(this.lastConnection).toBeDefined();
+    expect(this.lastConnection.request.url).toBe('api/companies/?minRating=4.4');
+  });
+
+  it('should query current service url with only minComments', () => {
+    this.CompanyFilterService.search(undefined, 3);
+    expect(this.lastConnection).toBeDefined();
+    expect(this.lastConnection.request.url).toBe('api/companies/?minComments=3');
+  });
+
+  it('should query current service url with both minRating and minComments', () => {
+    this.CompanyFilterService.search(4.4, 2);
+    expect(this.lastConnection).toBeDefined();
+    expect(this.lastConnection.request.url)
+    .toBe('api/companies/?minRating=4.4&minComments=2');
+  });
 });
--- glassdoor/src/app/companies/shared/company-filter.service.ts
+++ glassdoor/src/app/companies/shared/company-filter.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Http }       from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Company }		from './company.model';
 
 @Injectable()
 export class CompanyFilterService {
 
-  constructor() { }
+  constructor(private http: Http) {}
 
+  search(minRating?: number, minComments?: number): Observable<Company[]> {
+
+    let url = `api/companies/?`;
+    if (minRating && minComments) {
+      url += `minRating=${minRating}&minComments=${minComments}`;
+    }
+    else if (minRating)    url += `minRating=${minRating}`;
+    else if (minComments)  url += `minComments=${minComments}`;
+
+    return this.http
+               .get(url)
+               .map(response => response.json() as Company[]);
+  }
 }
