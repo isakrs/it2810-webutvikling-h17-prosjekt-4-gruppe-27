7dccb4c4ffd5cc9378952a85346d75cd9aadf2ea
Marius Maaland <mariusmaaland@gmail.com>
2017-11-21 16:47:00 +0100

    add conditional rendering of login or mypage

--- glassdoor/src/app/profile/profile.component.ts
+++ glassdoor/src/app/profile/profile.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoginComponent }			from './login/login.component';
+import { RegisterComponent }	from './register/register.component';
+import { MyPageComponent }		from './my-page/my-page.component';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+	isLoggedIn: boolean;
 
   constructor() { }
 
   ngOnInit() {
+  	this.isLoggedIn = false; // todo: get this from session(local storage)
   }
 
 }
