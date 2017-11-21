8f50924025d07762604c7e9cd59ce379504686b8
Marius Maaland <mariusmaaland@gmail.com>
2017-11-21 16:07:08 +0100

    add tests of the 3 different sorting attributes

--- glassdoor/src/app/companies/shared/sort-companies.pipe.spec.ts
+++ glassdoor/src/app/companies/shared/sort-companies.pipe.spec.ts
@@ -1,8 +1,42 @@
 import { SortCompaniesPipe } from './sort-companies.pipe';
 
+const companies = [
+  {_id: '1',   name: 'DNB',          averageRating: 3.4,   nComments: 13},
+  {_id: '2',   name: 'Bekk',         averageRating: null,  nComments: 12},
+  {_id: '3',   name: 'Kolonial.no',  averageRating: 4.1,   nComments: 0}
+];
+
 describe('SortCompaniesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortCompaniesPipe();
-    expect(pipe).toBeTruthy();
+
+	let sortCompaniesPipe: SortCompaniesPipe;
+
+	beforeEach(() => {
+    sortCompaniesPipe = new SortCompaniesPipe();
   });
+
+  it('create an instance', () => {
+    expect(sortCompaniesPipe).toBeTruthy();
+  });
+
+  it('should filter correctly on name', () => {
+  	const filtered = sortCompaniesPipe.transform(companies, 'name');
+  	expect(filtered.length).toBe(3);
+  	expect(filtered[0].name).toBe('Bekk');
+  	expect(filtered[2].name).toBe('Kolonial.no');
+  });
+
+  it('should filter correctly on averageRating', () => {
+  	const filtered = sortCompaniesPipe.transform(companies, 'averageRating');
+  	expect(filtered.length).toBe(3);
+  	expect(filtered[0].name).toBe('Kolonial.no');
+  	expect(filtered[2].name).toBe('Bekk');
+  });
+
+  it('should filter correctly on numComment', () => {
+  	const filtered = sortCompaniesPipe.transform(companies, 'nComments');
+  	expect(filtered.length).toBe(3);
+  	expect(filtered[0].name).toBe('DNB');
+  	expect(filtered[2].name).toBe('Kolonial.no');
+  });
+
 });
