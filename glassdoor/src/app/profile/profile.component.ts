86c35072f59ee4bd7f4f24185e6889e040d4c8bf
Marius Maaland <mariusmaaland@gmail.com>
2017-11-21 18:47:40 +0100

    add session reading from storage, and interfacing with profile service [not tested]

--- glassdoor/src/app/profile/profile.component.ts
+++ glassdoor/src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { LoginComponent }			from './login/login.component';
 import { MyPageComponent }		from './my-page/my-page.component';
+import { User }								from './shared/user.model';
 
 @Component({
   selector: 'app-profile',
@@ -10,11 +11,27 @@ import { MyPageComponent }		from './my-page/my-page.component';
 })
 export class ProfileComponent implements OnInit {
 	isLoggedIn: boolean;
+	auth: boolean;
+	user: User;
+	token: string;
 
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit() {
-  	this.isLoggedIn = false; // todo: get this from session(local storage)
+  	const session = JSON.parse(localStorage.getItem('session'));
+    this.checkSession(session);
   }
+
+	checkSession(session: any): void {
+		if (session === null || session.auth === false) {
+			this.isLoggedIn = false;
+		} else {
+			// this.isLoggedIn = this.profileService.validateToken(session.token);
+			this.auth = session.auth;
+			this.user = session.user;
+			this.token = session.token;
+		}
+	}
 
 }
