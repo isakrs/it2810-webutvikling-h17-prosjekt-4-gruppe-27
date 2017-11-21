14916bb7e6eb00d46a06fa3f298271d2e616a308
Johannes Z. Vollan <johannzv@stud.ntnu.no>
2017-11-21 10:04:04 +0100

    add tests for filter

--- backend/src/controllers/company/company.spec.ts
+++ backend/src/controllers/company/company.spec.ts
@@ -6,6 +6,7 @@ import {default as app} from './../../app'
 import * as _ from 'lodash'
 import Company from './../../db/models/companyModel'
 
+
 let companyName = 'testTestTest1'
 let companyID = ''
 
@@ -16,6 +17,31 @@ describe('testing api/company', function(){
 })
 
 describe('testing GET api/company',function(){
+    let comp1:any = new Company({
+        name:'name1',
+        averageRating: 1,
+        nComments: 5
+    })
+
+    let comp2:any = new Company({
+        name:'name2',
+        averageRating: 2,
+        nComments: 2
+    })
+    let comp3:any = new Company({
+        name:'name3',
+        averageRating: 1,
+        nComments: 6
+    })
+    before(async function(){
+        await Promise.all([comp1.save(), comp2.save(), comp3.save()])
+
+    })
+
+    after(async function(){
+        await Promise.all([Company.findByIdAndRemove(comp1._id), Company.findByIdAndRemove(comp2._id), Company.findByIdAndRemove(comp3._id)])
+    })
+
     it('should return a list of companies', function(){
        return supertest(app)
         .get('/api/company')
@@ -24,6 +50,51 @@ describe('testing GET api/company',function(){
             expect(response.body, 'is not an array').to.be.an('array')
         })
     })
+
+    it('should rank on minComments if its given in the queryString', async function(){
+        let response = await supertest(app).get('/api/company/?minComments=5').expect(200)
+        expect(response.body).to.be.an('array')
+        expect(response.body).to.have.lengthOf(2)
+        expect(response.body).to.have.deep.members([
+            {_id:comp1._id.toString(),
+            averageRating:comp1.averageRating,
+            nComments:comp1.nComments,
+            name:comp1.name
+            }, 
+            {_id:comp3._id.toString(),
+                averageRating:comp3.averageRating,
+                nComments:comp3.nComments,
+                name:comp3.name}
+            ])
+    })
+
+    it('should rank on minRating if its given in the queryString', async function(){
+        let response = await supertest(app).get('/api/company/?minRating=2').expect(200)
+        expect(response.body).to.be.an('array')
+        expect(response.body).to.have.lengthOf(1)
+        expect(response.body).to.have.deep.members([
+            {_id:comp2._id.toString(),
+            averageRating:comp2.averageRating,
+            nComments:comp2.nComments,
+            name:comp2.name
+            }
+            ])
+    })
+
+    it('should rank on both minRating and minComments', async function(){
+        let response = await supertest(app).get('/api/company/?minRating=1&minComments=6').expect(200)
+        expect(response.body).to.be.an('array')
+        expect(response.body).to.have.lengthOf(1)
+        expect(response.body).to.have.deep.members([
+            {_id:comp3._id.toString(),
+            averageRating:comp3.averageRating,
+            nComments:comp3.nComments,
+            name:comp3.name
+            }
+            ])
+    })
+
+    
 })
 
 describe('testing POST api/company ', function(){
