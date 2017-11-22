3dbc0f29e55b74d0042e55969bf9f3a7f02bc78d
Marius Maaland <mariusmaaland@gmail.com>
2017-11-22 20:26:21 +0100

    implement conditional rendering of company add field
    only visible when logged in

--- glassdoor/src/app/companies/companies.component.ts
+++ glassdoor/src/app/companies/companies.component.ts
@@ -5,6 +5,7 @@ import { Company }                from './shared/company.model';
 import { CompanyService }         from './shared/company.service';
 import { SortCompaniesPipe }      from './shared/sort-companies.pipe';
 import { CompanyFilterComponent}  from './company-filter/company-filter.component';
+import { ProfileService }         from '../profile/shared/profile.service';
 
 @Component({
   selector: 'my-companies',
@@ -16,10 +17,12 @@ export class CompaniesComponent implements OnInit {
   companies: Company[];
   selectedCompany: Company;
   sortValue: string;
+  isLoggedIn: boolean;
 
   constructor(
     private companyService: CompanyService,
-    private router:         Router
+    private router:         Router,
+    private profileService: ProfileService
   ) { }
 
   getCompanies(filter?): void {
@@ -62,10 +65,21 @@ export class CompaniesComponent implements OnInit {
   ngOnInit(): void {
     this.getCompanies();
     this.sortValue = "name";
+    const session = JSON.parse(localStorage.getItem('session'));
+    this.checkSession(session);
   }
 
   onSelect(company: Company): void {
     this.selectedCompany = company;
     this.router.navigate(['/detail', this.selectedCompany._id]);
+  }
+
+  private checkSession(session: any): void {
+    if (session === null) {
+      this.isLoggedIn = false;
+    } else {
+      this.profileService.isTokenValid(session.token)
+        .then(isLoggedIn => this.isLoggedIn = isLoggedIn);
+    }
   }
 }
