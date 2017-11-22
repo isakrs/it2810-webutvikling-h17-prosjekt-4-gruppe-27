c98f043d20a8ea49a7692ff1884b33c5951cbaa6
Marius Maaland <mariusmaaland@gmail.com>
2017-11-22 10:41:12 +0100

    add providers, imports and declarations to login component spec

--- glassdoor/src/app/profile/login/login.component.spec.ts
+++ glassdoor/src/app/profile/login/login.component.spec.ts
@@ -1,6 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed }     from '@angular/core/testing';
+import { RouterModule }                         from '@angular/router';
+import { Http, HttpModule }                     from '@angular/http';
+import { APP_BASE_HREF }                        from '@angular/common';
 
-import { LoginComponent } from './login.component';
+import { LoginComponent }     from './login.component';
+import { MyPageComponent }    from '../my-page/my-page.component';
+
+import { ProfileService }     from '../shared/profile.service';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +15,15 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent, MyPageComponent ],
+      providers: [
+        ProfileService,
+        {provide: APP_BASE_HREF, useValue: '/'}
+      ],
+      imports: [
+        RouterModule.forRoot([]),
+        HttpModule
+      ]
     })
     .compileComponents();
   }));
