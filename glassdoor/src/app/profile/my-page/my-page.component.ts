b19ed14dfa203855931c84e7865659eeb2bfecf1
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-22 14:50:21 +0100

    Display reviews on my-page. Delete functionality is also added.

--- glassdoor/src/app/profile/my-page/my-page.component.ts
+++ glassdoor/src/app/profile/my-page/my-page.component.ts
@@ -11,7 +11,9 @@ import { Review }         from '../../companies/reviews/shared/review.model';
 })
 export class MyPageComponent implements OnInit {
 	@Input() username: string;
-  reviews: Review[];
+
+  reviews:        Review[];
+  selectedReview: Review;
 
   constructor(private reviewService: ReviewService) { }
 
@@ -23,6 +25,19 @@ export class MyPageComponent implements OnInit {
     this.reviewService
         .getReviewsOfUser()
         .then(reviews => this.reviews = reviews);
+  }
+
+  delete(review: Review): void {
+    this.reviewService
+        .delete(review._id)
+        .then(() => {
+          this.reviews = this.reviews.filter(r => r !== review);
+          if (this.selectedReview === review) { this.selectedReview = null; }
+        });
+  }
+
+  onSelect(review: Review): void {
+    this.selectedReview = review;
   }
 
 }
