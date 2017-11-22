01da6054c4f635e839da2ad4fc964267ef72917d
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-11-22 16:20:20 +0100

    Change e2e test add and remove company.
    A user has to be loged in and out to the these changes now.

--- glassdoor/e2e/companies.e2e-spec.ts
+++ glassdoor/e2e/companies.e2e-spec.ts
@@ -1,18 +1,29 @@
 import { browser, protractor, element, by } from 'protractor';
 
 import { CompaniesPage } from './companies.po';
+import { ProfilePage } from './profile.po';
 
 describe('Companies page', () => {
 
   let cPage:      CompaniesPage;
   let nCompanies: number;
 
+  let pPage:      ProfilePage;
+  const existingUser = {username: 'testRobot', password: 'passTestRobot'};
+
   beforeEach(async() => {
     cPage = new CompaniesPage();
+    pPage = new ProfilePage();
   });
 
-  it('should add and remove companies', async() => {
+  it('should let user add and remove companies', async() => {
 
+    // log in user
+    pPage.navigateTo();
+    pPage.login(existingUser);
+    browser.sleep(10000);
+
+    // navigate to companies page
     cPage.navigateTo();
 
     // Store number of companies at this time
@@ -20,6 +31,7 @@ describe('Companies page', () => {
       nCompanies=num
     });
 
+    // check that there exists companies
     expect(cPage.noCompanies()).toBeGreaterThan(0);
 
     // add a test company
@@ -29,6 +41,9 @@ describe('Companies page', () => {
     // delete the added test company (will be the last index)
     cPage.delCompany(nCompanies);
     expect(cPage.noCompanies()).toBe(nCompanies);
+
+    // logout user
+    pPage.logout();
 
   });
 
