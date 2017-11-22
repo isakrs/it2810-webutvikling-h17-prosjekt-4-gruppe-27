eccf831b924b7d94675311ef2ad3b347abe4a7ee
Marius Maaland <mariusmaaland@gmail.com>
2017-11-22 12:51:01 +0100

    implement authorization header in company service

--- glassdoor/src/app/companies/shared/company.service.ts
+++ glassdoor/src/app/companies/shared/company.service.ts
@@ -7,13 +7,28 @@ import { environment }   from '../../../environments/environment';
 
 import { Company } from './company.model';
 
+
 @Injectable()
 export class CompanyService {
-
-  private headers = new Headers({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}, );
+  private token: string;
+  private session: any;
+  private headers: Headers;
   private companiesUrl = `${environment.apiUrl}/api/company`;  // URL to web api
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+    const session = JSON.parse(localStorage.getItem('session'));
+    if (session !== null) {
+      this.token = session.token;
+    }
+
+    // Initialize headers. Token will be undefined if user is not logged in, but that's fine
+    // for some requests
+    this.headers = new Headers(
+      {'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': `Bearer ${this.token}`}
+      );
+  }
 
   getCompanies(minRating?: number, minComments?: number): Promise<Company[]> {
 
@@ -23,9 +38,9 @@ export class CompanyService {
     else if (minRating  && minComments)  url += `/?minRating=${minRating}&minComments=${minComments}`;
 
     return this.http.get(url)
-               .toPromise()
-               .then(response => response.json() as Company[])
-               .catch(this.handleError);
+      .toPromise()
+      .then(response => response.json() as Company[])
+      .catch(this.handleError);
   }
 
   getCompany(id: string): Promise<Company> {
