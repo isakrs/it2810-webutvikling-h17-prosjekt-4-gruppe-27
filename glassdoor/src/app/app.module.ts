25648a41543af4eb6491dc0ed6787f92fd731c60
Marius Maaland <mariusmaaland@gmail.com>
2017-11-23 01:14:01 +0100

    fix styling on company detail component

--- glassdoor/src/app/app.module.ts
+++ glassdoor/src/app/app.module.ts
@@ -8,23 +8,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './shared/in-memory-data.service';
 
-import { AppComponent }           from './app.component';
-import { CompaniesComponent }     from './companies/companies.component';
-import { CompanyDetailComponent } from './companies/company-detail/company-detail.component';
-import { CompanySearchComponent } from './companies/company-search/company-search.component';
-import { DashboardComponent }     from './dashboard/dashboard.component';
-import { CompanyService }         from './companies/shared/company.service';
-import { ReviewDetailComponent }  from './companies/reviews/review-detail/review-detail.component';
-import { ReviewsComponent }       from './companies/reviews/reviews.component';
-import { SortCompaniesPipe }      from './companies/shared/sort-companies.pipe';
-import { CompanyFilterComponent } from './companies/company-filter/company-filter.component';
+import { AppComponent }            from './app.component';
+import { CompaniesComponent }      from './companies/companies.component';
+import { CompanyDetailComponent }  from './companies/company-detail/company-detail.component';
+import { CompanySearchComponent }  from './companies/company-search/company-search.component';
+import { DashboardComponent }      from './dashboard/dashboard.component';
+import { CompanyService }          from './companies/shared/company.service';
+import { ReviewDetailComponent }   from './companies/reviews/review-detail/review-detail.component';
+import { ReviewsComponent }        from './companies/reviews/reviews.component';
+import { SortCompaniesPipe }       from './companies/shared/sort-companies.pipe';
+import { CompanyFilterComponent }  from './companies/company-filter/company-filter.component';
 
-import { ProfileComponent }       from './profile/profile.component';
-import { LoginComponent }         from './profile/login/login.component';
-import { MyPageComponent }        from './profile/my-page/my-page.component';
-import { ProfileService }         from './profile/shared/profile.service';
-import { LogoutComponent }        from './profile/logout/logout.component';
-
+import { ProfileComponent }        from './profile/profile.component';
+import { LoginComponent }          from './profile/login/login.component';
+import { MyPageComponent }         from './profile/my-page/my-page.component';
+import { ProfileService }          from './profile/shared/profile.service';
+import { LogoutComponent }         from './profile/logout/logout.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule,
@@ -33,8 +32,11 @@ import { MatButtonModule,
          MatIconModule,
          MatInputModule,
          MatAutocompleteModule,
-         MatSelectModule
+         MatSelectModule,
+         MatSliderModule
        } from '@angular/material';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -67,7 +69,8 @@ import { MatButtonModule,
     MatIconModule,
     MatInputModule,
     MatAutocompleteModule,
-    MatSelectModule
+    MatSelectModule,
+    MatSliderModule
   ],
   providers: [ CompanyService, ProfileService ],
   bootstrap: [ AppComponent ]
